4ae149c775ccb868b3589e798d9511b5c3543c98
Mayra Amezcua <mamezcua.mma@gmail.com>
2020-09-03 14:46:36 -0700

    Separate functions

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const myInput1 = document.getElementById("sum-input-1");
 const myInput2 = document.getElementById("sum-input-2");
+const myInput3 = document.getElementById("sum-input-3");
+const myInput4 = document.getElementById("sum-input-4");
+const myInput5 = document.getElementById("sum-input-5");
+const myInput6 = document.getElementById("sum-input-6");
+const myInput7 = document.getElementById("sum-input-7");
+const myInput8 = document.getElementById("sum-input-8");
 const mySumButton = document.getElementById("sum-button");
 const mySubButton = document.getElementById("sub-button");
 const myMulButton = document.getElementById("mul-button");
 const myDivButton = document.getElementById("div-button");
-const mySpan = document.getElementById("sum-span");
+const mySumSpan = document.getElementById("sum-span");
+const mySubSpan = document.getElementById("sub-span");
+const myMulSpan = document.getElementById("mul-span");
+const myDivSpan = document.getElementById("div-span");
 
 // console.log(myInput1)
 // console.log(myInput2)
@@ -20,26 +29,26 @@ mySumButton.addEventListener('click', () => {
     // const sumValue = parseFloat(value1) + parseFloat(value2);
     const sumValue = value1 + value2;
     // console.log(sumValue);
-    mySpan.textContent = sumValue;
+    mySumSpan.textContent = sumValue;
 })
 
 mySubButton.addEventListener('click', () => {
-    const value1 = myInput1.valueAsNumber;
-    const value2 = myInput2.valueAsNumber;
-     const subValue = value1 - value2;
-    mySpan.textContent = subValue;
+    const value3 = myInput3.valueAsNumber;
+    const value4 = myInput4.valueAsNumber;
+     const subValue = value3 - value4;
+    mySubSpan.textContent = subValue;
 })
 
 myMulButton.addEventListener('click', () => {
-    const value1 = myInput1.valueAsNumber;
-    const value2 = myInput2.valueAsNumber;
-    const mulValue = value1 * value2;
-    mySpan.textContent = mulValue;
+    const value5 = myInput5.valueAsNumber;
+    const value6 = myInput6.valueAsNumber;
+    const mulValue = value5 * value6;
+    myMulSpan.textContent = mulValue;
 })
 
 myDivButton.addEventListener('click', () => {
-    const value1 = myInput1.valueAsNumber;
-    const value2 = myInput2.valueAsNumber;
-    const divValue = value1 / value2;
-    mySpan.textContent = divValue;
+    const value7 = myInput7.valueAsNumber;
+    const value8 = myInput8.valueAsNumber;
+    const divValue = value7 / value8;
+    myDivSpan.textContent = divValue;
 })
